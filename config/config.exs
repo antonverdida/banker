480ad860fac3a6d6e5b1cba898f4fc4701c08023
@Anton <verdidaantonio7@gmail.com>
2026-01-15 11:25:33 +0800

    Generate User Auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :banker, :scopes,
+  user: [
+    default: true,
+    module: Banker.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Banker.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :banker,
   ecto_repos: [Banker.Repo],
   generators: [timestamp_type: :utc_datetime]
